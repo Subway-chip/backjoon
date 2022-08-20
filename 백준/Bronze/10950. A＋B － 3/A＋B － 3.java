39291127116280396d7dcb57d2bb045137943b28
Subway-chip <80993793+Subway-chip@users.noreply.github.com>
2022-08-20 23:37:32 +0900

    Update A＋B － 3.java

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -20,6 +20,20 @@ public class Main {
 			System.out.println(k);
 		}
 		
+		 /*
+		case 2
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int i=0;i<T;i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println(A + B);
+		}
+        
+	     */
+		
 	}
 
 }
