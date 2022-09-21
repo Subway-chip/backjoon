2610829b294ed3c9ead004be46e012552e1cb919
Subway-chip <80993793+Subway-chip@users.noreply.github.com>
2022-09-21 23:20:25 +0900

    [Bronze V] Title: X보다 작은 수, Time: 452 ms, Memory: 27196 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		int A[] = new int[N]; 
+		
+		for(int i = 0 ; i < N ; i++) {
+			A[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		for(int i = 0; i < N ; i++) {
+			if(A[i] < X) {
+				System.out.print(A[i] + " ");
+			}
+		}
+		
+	}
+
+}
