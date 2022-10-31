687e8f3df23eccab258a4fc2ca0936613896edfa
Subway-chip <80993793+Subway-chip@users.noreply.github.com>
2022-10-31 22:37:58 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 112 ms, Memory: 12812 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int y = sc.nextInt();
+		
+		if(2018 <= y && y <= 2022) {
+			System.out.println("A");
+		}
+		
+		
+		
+	
+	
+	}
+}
