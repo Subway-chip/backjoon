e5c4237ac65fd19a1a9ce21f737c1b256ad4bc02
Subway-chip <80993793+Subway-chip@users.noreply.github.com>
2022-11-25 15:13:07 +0900

    [Bronze IV] Title: 자동완성, Time: 108 ms, Memory: 12768 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String input = sc.next();
+		
+		if(input.equalsIgnoreCase("n")) {
+			System.out.println("Naver D2");
+		}else {
+			System.out.println("Naver Whale");
+		}
+
+	}
+}
