89f3ba9ca4eb1a64f987b1e010e446807e837950
Subway-chip <80993793+Subway-chip@users.noreply.github.com>
2022-12-13 23:39:43 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 308 ms, Memory: 17932 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -10,11 +10,11 @@ public class Main {
 		
 		for(int i = 1 ; i <= N; i++) {
 			
-			for(int k = i ; k < N; k++) {
+			for(int j = i ; j < N; j++) {
 				System.out.print(" ");
 			}
 			
-			for(int j = 0 ; j < 2*i-1; j++) {
+			for(int k = 0 ; k < 2*i-1; k++) {
 				System.out.print("*");
 			}
 			System.out.println();
