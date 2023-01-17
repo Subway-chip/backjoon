bb904d61bb8fe5ec938f60cd4e105e75a55ebaaf
Subway-chip <80993793+Subway-chip@users.noreply.github.com>
2023-01-18 00:03:25 +0900

    [Bronze II] Title: TV 크기, Time: 112 ms, Memory: 12872 KB -BaekjoonHub

--- 백준/Bronze/1297. TV 크기/TV 크기.java
+++ 백준/Bronze/1297. TV 크기/TV 크기.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
-class Main{
+
+public class Main {
+
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
+
 		int D = sc.nextInt();
-		int H_rate = sc.nextInt();
-		int W_rate  = sc.nextInt();
-		double answer = Math.sqrt(1.0*D*D/(H_rate*H_rate+W_rate*W_rate));
-		System.out.println((int) Math.floor(answer*H_rate)+" "+(int) Math.floor(answer*W_rate));
-		sc.close();
-	}    
+		int H = sc.nextInt();
+		int W = sc.nextInt();
+
+		double val = Math.sqrt(1.0 * D * D / (H * H + W * W));
+
+		System.out.print((int) Math.floor(H * val) + " " + (int) Math.floor(W * val));
+
+	}
 }
