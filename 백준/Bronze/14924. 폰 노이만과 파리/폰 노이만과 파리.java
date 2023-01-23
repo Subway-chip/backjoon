104eaa6dc1f8235d06ea47905f281b40f12658a8
Subway-chip <80993793+Subway-chip@users.noreply.github.com>
2023-01-23 23:49:14 +0900

    [Bronze IV] Title: 폰 노이만과 파리, Time: 104 ms, Memory: 12812 KB -BaekjoonHub

---
+++ 백준/Bronze/14924. 폰 노이만과 파리/폰 노이만과 파리.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int S = sc.nextInt();
+		int T = sc.nextInt();
+		int D = sc.nextInt();
+		int time = D/(S*2);
+		int F = time*T;
+		
+		System.out.println(F);
+
+	}
+
+}
