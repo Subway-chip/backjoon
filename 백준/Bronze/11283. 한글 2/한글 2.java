10d4adf24e525121a75ff682af8db22799c236de
Subway-chip <80993793+Subway-chip@users.noreply.github.com>
2023-02-18 00:00:46 +0900

    [Bronze IV] Title: 한글 2, Time: 104 ms, Memory: 12888 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        char A=sc.next().charAt(0);
+        int B=(int) A;
+        System.out.println(B-44031);
+    }
+                                          
+}
