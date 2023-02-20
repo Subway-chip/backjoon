eb0d389c90fcd16fe367102affe434b249f5783d
Subway-chip <80993793+Subway-chip@users.noreply.github.com>
2023-02-20 23:50:08 +0900

    [Bronze IV] Title: Next in line, Time: 116 ms, Memory: 12812 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int y = sc.nextInt();
+		int m = sc.nextInt();
+		int o = 2*m-y;
+		
+		System.out.println(o);
+		
+		
+
+	}
+}
