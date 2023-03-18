77e316aa579e5999c8fd16256c3436658c2a9818
Subway-chip <80993793+Subway-chip@users.noreply.github.com>
2023-03-18 23:46:54 +0900

    [Bronze III] Title: 더하기, Time: 104 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		int sum = 0;
+
+		for (int i = 0; i < T; i++) {
+			int N = sc.nextInt();
+			for (int j = 0; j < N; j++) {
+				int num = sc.nextInt();
+				sum += num;
+			}
+			System.out.println(sum);
+			sum = 0;
+		}
+
+	}
+}
